656e824ce8cb43ad7ad3d7deea0912491c3f1463
zhengsr <845731923@qq.com>
2021-11-10 17:49:41 +0800

    处理不同步的问题

--- app/src/main/java/com/zhengsr/nfcdemo/MainActivity.kt
+++ app/src/main/java/com/zhengsr/nfcdemo/MainActivity.kt
@@ -8,10 +8,13 @@ import android.widget.TextView
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.chad.library.adapter.base.BaseQuickAdapter
 import com.chad.library.adapter.base.viewholder.BaseViewHolder
+import com.drake.brv.utils.linear
+import com.drake.brv.utils.setup
 import com.excshare.nfclib.NfcApi
 import com.excshare.nfclib.bean.NfcBean
 import com.excshare.nfclib.bean.ScreenNfcBean
 import com.excshare.nfclib.callback.INfcReadListener
+import com.excshare.nfclib.callback.INfcWriteListener
 import com.excshare.nfclib.status.NfcErrorCode
 import kotlinx.android.synthetic.main.activity_main.*
 import org.jetbrains.annotations.NotNull
@@ -19,16 +22,13 @@ import kotlin.experimental.and
 
 class MainActivity : AppCompatActivity() {
     private  val TAG = "MainActivity"
-    private lateinit var cardAdapter:CardAdapter
     private val datas = mutableListOf<CardBean>()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         NfcApi.getScreenNfc().makeActivityEnableForeground(this)
-
-        card_recy.layoutManager = LinearLayoutManager(this)
-        cardAdapter = CardAdapter(R.layout.item_layout,datas);
-        card_recy.adapter = cardAdapter
+        //NfcApi.getScreenNfc().config("https://r302.c/e1r2Xrz","com.seewo.cn",null);
+        initRecycleView()
         readNfc(intent)
 
     }
@@ -38,32 +38,37 @@ class MainActivity : AppCompatActivity() {
         readNfc(intent)
     }
     private fun readNfc(intent: Intent?){
-
+        datas.clear()
         intent?.let {
             if (NfcApi.getScreenNfc().isIntentSupportNfc(it)) {
                 NfcApi.getScreenNfc().readNfc(it, object : INfcReadListener {
                     override fun onFail(code: NfcErrorCode?, errorMsg: String?) {
-                        Log.d(TAG, "zsr onFail: $code,$errorMsg")
-                        datas.add(CardBean("Ssid: ",""))
-                        datas.add(CardBean("password: ",""))
-                        datas.add(CardBean("apIp: ",""))
-                        datas.add(CardBean("port: ",""))
-                        datas.add(CardBean("hideSsid: ",""))
-                        datas.add(CardBean("hidePassword: ",""))
-                        datas.add(CardBean("downloadUrl: ",""))
-                        datas.add(CardBean("packageName: ",""))
-                        cardAdapter.notifyDataSetChanged()
+                        runOnUiThread {
+                            Log.d(TAG, "zsr onFail: $code,$errorMsg")
+                            datas.add(CardBean("Ssid: ", ""))
+                            datas.add(CardBean("password: ", ""))
+                            datas.add(CardBean("apIp: ", ""))
+                            datas.add(CardBean("port: ", ""))
+                            datas.add(CardBean("hideSsid: ", ""))
+                            datas.add(CardBean("hidePassword: ", ""))
+                            datas.add(CardBean("downloadUrl: ", ""))
+                            datas.add(CardBean("packageName: ", ""))
+                            card_recy.adapter?.notifyDataSetChanged()
+                        }
                     }
 
                     override fun onSuccess(bean: NfcBean?) {
-                        bean?.apply {
-                            datas.add(CardBean("Ssid: ",ssid))
-                            datas.add(CardBean("password: ",password))
-                            datas.add(CardBean("apIp: ",apIp))
-                            datas.add(CardBean("port: ","$port"))
-                            datas.add(CardBean("hideSsid: ",hideSsid))
-                            datas.add(CardBean("hidePassword: ",hidePassword))
-                            cardAdapter.notifyDataSetChanged()
+                        Log.d(TAG, "zsr onSuccess: $bean , ${bean?.password}")
+                        runOnUiThread {
+                            bean?.apply {
+                                datas.add(CardBean("Ssid: ", ssid))
+                                datas.add(CardBean("password: ", password))
+                                datas.add(CardBean("apIp: ", apIp))
+                                datas.add(CardBean("port: ", "$port"))
+                                datas.add(CardBean("hideSsid: ", hideSsid))
+                                datas.add(CardBean("hidePassword: ", hidePassword))
+                              //  card_recy.adapter?.notifyDataSetChanged()
+                            }
                         }
                     }
 
@@ -74,13 +79,11 @@ class MainActivity : AppCompatActivity() {
                                 card_id.text = "卡号: ${nfcId.toHexString()}"
                                 card_size.text = "总容量: $maxSize kb，已写入: $payloadSize kb"
 
-                                card_size.postDelayed({
-                                    datas.add(CardBean("downloadUrl: ",url))
-                                    if (!pkgLists.isNullOrEmpty()) {
-                                        datas.add(CardBean("packageName: ",pkgLists[0]))
-                                    }
-                                    cardAdapter.notifyDataSetChanged()
-                                },200)
+                                datas.add(CardBean("downloadUrl: ",url))
+                                if (!pkgLists.isNullOrEmpty()) {
+                                    datas.add(CardBean("packageName: ",pkgLists[0]))
+                                }
+                                card_recy.adapter?.notifyDataSetChanged()
                             }
                         }
                     }
@@ -88,6 +91,28 @@ class MainActivity : AppCompatActivity() {
                 })
             }
         }
+    }
+
+    private fun  initRecycleView(){
+        card_recy.linear().setup {
+            addType<CardBean>(R.layout.item_layout)
+            onBind {
+                val titleTv = findView<TextView>(R.id.item_title)
+                val msgTv = findView<TextView>(R.id.item_msg)
+                val title = getModel<CardBean>().title
+                val msg = getModel<CardBean>().msg
+                msg?.let {
+                    msgTv.text = it
+                }
+                titleTv.text = title
+                if (msg.isNullOrEmpty() || msg == "0"){
+                    titleTv.setTextColor(this@MainActivity.resources.getColor(R.color.read))
+                }else{
+                    titleTv.setTextColor(this@MainActivity.resources.getColor(R.color.teal_700))
+                }
+
+            }
+        }.models = datas
     }
 
     override fun onResume() {
@@ -100,21 +125,8 @@ class MainActivity : AppCompatActivity() {
         NfcApi.getScreenNfc().disableForegroundDispatch(this)
     }
 
+
+
 }
 fun ByteArray.toHexString() = joinToString("") { "%02x".format(it) }
 data class CardBean(var title:String,var msg:String?)
-class CardAdapter(val layoutId:Int,val datas:MutableList<CardBean>)
-    : BaseQuickAdapter<CardBean,BaseViewHolder>(layoutId,datas){
-    override fun convert(holder: BaseViewHolder, item: CardBean) {
-        holder.setText(R.id.item_title,item.title);
-        item.msg?.let {
-            holder.setText(R.id.item_msg,it)
-        }
-        if (item.msg.isNullOrEmpty()){
-            holder.setTextColorRes(R.id.item_title,R.color.read)
-        }else{
-            holder.setTextColorRes(R.id.item_title,R.color.teal_700)
-        }
-    }
-
-}
